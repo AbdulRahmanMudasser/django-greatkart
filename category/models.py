1e8e63c1730e9662bf3f5aeef80b97a8958b9f24
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-28 11:52:03 +0500

    category model, registering category model, added and installed package, run migrations

--- category/models.py
+++ category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Category Model
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    category_image = models.ImageField(upload_to='images/categories', blank=True)
+    
+    def __str__(self):
+        return self.category_name
