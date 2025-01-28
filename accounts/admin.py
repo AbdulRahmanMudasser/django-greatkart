45a10e766e2176e6c9fb4c9b984dce4d871ddce1
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-28 13:04:06 +0500

    admin modifications for hashing and readonly password, creating links, making fields readonly, registering account admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
+class AccountAdmin(UserAdmin):
+    # Fields to Display
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    
+    # Links 
+    list_display_links = ('email', 'first_name', 'last_name')
+    
+    # Readonly Fields
+    readonly_fields = ('last_login', 'date_joined')
+    
+    # In Descending Order
+    ordering = ('-date_joined',)
+    
+    # Because We Are Using Custom User Model
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
 # Register Account Model
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
