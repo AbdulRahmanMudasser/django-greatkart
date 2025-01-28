70a1a1c6c8017396524e561dd57ecd676f1181fd
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-28 16:22:40 +0500

    handling media files, configured urls.py

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -7,4 +10,4 @@ urlpatterns = [
     
     # Home URL
     path('', views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
