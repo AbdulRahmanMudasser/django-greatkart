467433aadfc22d0cefd867f908cee23dddde059f
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-01-29 10:39:41 +0500

    product model, registering product model and product admin, configuring admin.py file to prepopulate slug field, disply list of fields

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from category.models import Category
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.IntegerField()
+    discount_percentage = models.FloatField(default=0.0)
+    discount_amount = models.FloatField(default=0.0) 
+    images = models.ImageField(upload_to='images/products', blank=True)
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def calculate_discount(self):
+        price_after_percentage = self.price - (self.price * (self.discount_percentage / 100))
+        price_after_fixed_discount = self.price - self.discount_amount
+        return max(0, min(price_after_percentage, price_after_fixed_discount))
+
+    @property
+    def final_price(self):
+        return self.calculate_discount()
+
+    def __str__(self):
+        return self.product_name
