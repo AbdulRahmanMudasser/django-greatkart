892a2c00da20567775d42d7dabf8d9f0c0f5f89d
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-02-15 18:49:14 +0500

    add to cart login, getting and creating user session, testing

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # Cart URL
     path('', views.cart, name='cart'),
+    
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,41 +1,59 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from store.models import Product
 from .models import Cart, CartItem
 
 # Get Session Key
 def _get_session(request):
-    # Get Session Key
+    # Get Session Key of User
     session_id = request.session.session_key
     
-    # Check if Session is not Created yet
+    # Check if Session Key Does Not Exists
     if not session_id:
-        # Create a New Session
+        # Create  New Session
         session_id = request.session.create()
         
-    # Return Session Id
+    # Return Session Key
     return session_id
 
 # Add to Cart Function
-def cart_view(request, product_id):
+def add_cart(request, product_id):
     # Get Product By id
     product = Product.objects.get(id=product_id)
     
     try:
+        # Find Existing Cart
         cart = Cart.objects.get(cart_id=_get_session(request))
     except Cart.DoesNotExist:
+        # Create New Cart
         cart = Cart.objects.create(cart_id=_get_session(request))
         
+        # Save Cart
         cart.save()
         
     try:
+        # Find Product in Cart
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        
+        # if Product Exists, Increase Quantity
         cart_item.quantity += 1
+        
+        # Save Cart Prodcut
         cart_item.save()
     except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+        # if Product does not Exits in Cart, Create Product
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart, is_active=True)
+        
+        # Save Product
         cart_item.save()
         
-    return redirect()
+    # Check
+    # return HttpResponse(cart_item.product)
+
+    # exit()
+        
+    # Redirect to Cart
+    return redirect('cart')
         
 
 # Cart View
