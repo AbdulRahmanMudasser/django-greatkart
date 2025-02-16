4d5bef420e6de8e48584100c5172db2cba4f813d
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-02-16 12:15:39 +0500

    implementing cart items, calculating subtotal, template configurations

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -12,8 +13,11 @@ def _get_session(request):
     
     # Check if Session Key Does Not Exists
     if not session_id:
-        # Create  New Session
-        session_id = request.session.create()
+        # Create New Session
+        request.session.create()
+        
+        # Retrieve Session Key
+        session_id = request.session.session_key
         
     # Return Session Key
     return session_id
@@ -59,28 +63,32 @@ def add_cart(request, product_id):
         
 
 # Cart View
-def cart(request, total=0, quantity=0, cart_items=None):
+def cart(request, total=Decimal('0.00'), quantity=0, cart_items=None):
     try:
         # Get Cart Associated With Current User Session
         cart = Cart.objects.get(cart_id=_get_session(request))
-        
+
         # Retrieve All Active Cart Items for Cart
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-        
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True) or []
+
         # Calculate Total Price & Quantity of Items in Cart
         for cart_item in cart_items:
-            total += (cart_item.product.price * cart_item.quantity)
-            quantity +=cart_item.quantity
-    
+            total += Decimal(str(cart_item.product.discounted_price)) * cart_item.quantity
+            quantity += cart_item.quantity
+            
+            print(f"Total: {total}, Quantity: {quantity}, Cart Items: {cart_item.product}")
+
+
     except ObjectDoesNotExist:
-        # if Cart or Cart Items Does Not Exist, Do Nothing 
-        pass
-    
+        # If Cart or Cart Items Do Not Exist, Ensure cart_items is an Empty List
+        cart_items = []
+
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
     }
-    
+
     # Render Cart Template
     return render(request, 'cart/cart.html', context)
+
