5d14dfd28cc25dbca3777852cc6a32874d3ae4ba
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-02-17 12:53:52 +0500

    check if product is already in cart, view cart button, added to cart infor

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+
+from cart.models import CartItem
+from cart.views import _get_session
 from .models import Product
 from category.models import Category
 
@@ -28,11 +31,16 @@ def product_details(request, category_slug=None, product_slug=None):
     try:
         # Try to Get Product By Category
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        
+        # Checking if Item is in Cart
+        in_cart = CartItem.objects.filter(cart__cart_id=_get_session(request), product=single_product).exists()
+        
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     
     # Render Product Details Template
