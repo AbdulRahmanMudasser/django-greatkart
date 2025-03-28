217e1d25b0fb463c84921b217d47daf82dda6983
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-03-28 11:22:11 +0500

    accounts: form overriding and customization

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,29 @@ from .models import Account
 
 # Register User Form
 class RegisterUserForm(forms.ModelForm):
+    
+    # Password Field
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+    
+    # Confirm Password Field
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Confirm Password',
+    }))
+    
     class Meta:
         model = Account
-        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password', 'confirm_password']
+        
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+        
